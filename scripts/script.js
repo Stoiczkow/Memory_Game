079f513e171b0df10c3242b6eb8af8425876ee59
Pawel Wyzykowski <pawelwyzykowski@gmail.com>
2017-06-05 15:34:36 +0200

    minor fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+    var counter = 0;
     $('div').on('click', function(){
-        console.log('Kliknales!');
-        $(this).children().toggleClass('hidden');
+        $(this).children().toggleClass('hidden visible');
+        counter ++;
+        console.log(counter);
+        if(counter == 2){
+            var visible_imgs = $('img.visible');
+        }
     });
 });
