0750faa924f829c99a3a7a9bac1bb6b08b788e85
Pawel Wyzykowski <pawelwyzykowski@gmail.com>
2017-06-05 15:56:56 +0200

    added comparing imgs

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
         console.log(counter);
         if(counter == 2){
             var visible_imgs = $('img.visible');
+            var visible_list = ['',''];
+            visible_imgs.each(function(index, element){
+                visible_list[index] = $(this).attr('class');
+            });
+
+            if(visible_list[0] === visible_list[1]){
+                console.log('equal');
+            }else{
+                console.log('not equal');
+            };
+            counter = 0;
         }
     });
 });
